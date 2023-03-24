7933052c9741852e3e57f84bbf62d0dd73a5951b
pm-moyanor <104218745+pm-moyanor@users.noreply.github.com>
2023-03-24 13:56:18 -0400

    fix thumbnail, add texts/ replace section tags

--- ceramic-portfolio.js
+++ ceramic-portfolio.js
@@ -1,11 +1,38 @@
 const images = document.querySelectorAll('img');
 
-images.forEach(img => {
-  const parentDiv = document.createElement('div');
-  const h2 = document.createElement('h2');
-  h2.textContent = img.alt;
-  parentDiv.appendChild(img.cloneNode());
-  parentDiv.appendChild(h2);
-  parentDiv.classList.add('art-container')
-  img.parentNode.replaceChild(parentDiv, img);
-});
+const imageList = ["https://images.unsplash.com/photo-1565193566173-7a0ee3dbe261?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80",
+
+"https://images.unsplash.com/photo-1578749556568-bc2c40e68b61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80",
+
+"https://images.unsplash.com/photo-1610701596007-11502861dcfa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+
+"https://images.unsplash.com/photo-1576020799627-aeac74d58064?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2615&q=80",
+
+"https://images.unsplash.com/photo-1525974160448-038dacadcc71?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2274&q=80",
+
+"https://images.unsplash.com/photo-1610701596061-2ecf227e85b2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80",
+
+"https://images.unsplash.com/photo-1490312278390-ab64016e0aa9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80",
+
+"https://images.unsplash.com/photo-1605883705077-8d3d3cebe78c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80"
+]
+
+const thumbnails = ["Josua Vase","larkin Plates", "Fortessa Cloud No 2", "Astoria Collection","Lava Beach Stoneware","Sand Canyon Bowls","Lennon Collection","Mendocino Dinnerware"]
+
+function fillArt(images, titles) {
+    let index = 0;
+    images.forEach((img) => {
+      const parentDiv = document.createElement('div');
+      const h2 = document.createElement('h2');
+      h2.textContent = titles[index++];
+      h2.classList.add('title');
+      parentDiv.appendChild(img.cloneNode());
+      parentDiv.appendChild(h2);
+      parentDiv.classList.add('card');
+      img.parentNode.replaceChild(parentDiv, img);
+    });
+  }
+  
+
+fillArt(images,thumbnails)
+
